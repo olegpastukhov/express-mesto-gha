7092493299e0f314b0ae7c68f2f45e5a315457db
olegpastukhov <104835041+olegpastukhov@users.noreply.github.com>
2022-12-27 11:46:15 +0300

    15-sprint-update-controllers-users-update-2

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,8 @@ const login = async (req, res, next) => {
 
     const token = jwt.sign(
       { _id: user._id },
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+      { expiresIn: '7d' },
     );
     return res.send({ token });
   } catch (e) {
